c775ce5269b1c23cc5b673098236d2a204c6d5d5
Eugene <evgpavlov18@gmail.com>
2018-12-06 22:53:26 +0300

    init version 1.0.0

--- src/main/kotlin/tk/epavlov/microsystem/boot/common/WelcomePage.kt
+++ src/main/kotlin/tk/epavlov/microsystem/boot/common/WelcomePage.kt
@@ -1,15 +1,12 @@
 package tk.epavlov.microsystem.boot.common
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZoneOffset
 import java.time.temporal.ChronoUnit
 import javax.annotation.PostConstruct
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 class WelcomePage {
@@ -24,25 +21,12 @@ class WelcomePage {
     fun hello(): Uptime {
         val seconds = ChronoUnit.SECONDS.between(Instant.ofEpochMilli(startTime), Instant.ofEpochMilli(System.currentTimeMillis()))
         return Uptime(
-                seconds = seconds,
+                seconds = seconds%60,
                 minutes = seconds/60%60,
                 hours = seconds/60/60%24,
                 days = ChronoUnit.DAYS.between(Instant.ofEpochMilli(startTime), Instant.ofEpochMilli(System.currentTimeMillis())),
                 startDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(startTime), ZoneId.systemDefault())
         )
-    }
-
-    @PostMapping("restapi/track")
-    fun postTest(request: HttpServletRequest): String {
-        val list = request.headerNames.toList()
-        list.forEach {
-            println("$it: ${request.getHeader(it)}")
-        }
-        println()
-        request.parameterMap.forEach { t, u ->
-            println("$t: $u")
-        }
-        return "kuka"
     }
 
     data class Uptime(
