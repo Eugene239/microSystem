fb3127125dcee7e0d04654ac80b52ac469b80747
Eugene <evgpavlov18@gmail.com>
2018-12-06 08:28:05 +0300

    change welcome page

--- src/main/kotlin/tk/epavlov/microsystem/boot/common/WelcomePage.kt
+++ src/main/kotlin/tk/epavlov/microsystem/boot/common/WelcomePage.kt
@@ -3,14 +3,24 @@ package tk.epavlov.microsystem.boot.common
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import javax.annotation.PostConstruct
 import javax.servlet.http.HttpServletRequest
 
 @RestController
 class WelcomePage{
+    var startTime : Long = 0
+
+    @PostConstruct
+    fun init(){
+        startTime= System.currentTimeMillis()
+    }
 
     @GetMapping("/")
     fun hello(): String{
-        return "Hello, retard"
+        val seconds = ChronoUnit.SECONDS.between (Instant.ofEpochMilli(startTime),Instant.ofEpochMilli(System.currentTimeMillis()))
+        return "[UPTIME]: ${seconds/60/24}:${seconds/60%24} ${seconds%60}"
     }
 
     @PostMapping("restapi/track")
--- src/main/kotlin/tk/epavlov/microsystem/boot/rest/ParserController.kt
+++ src/main/kotlin/tk/epavlov/microsystem/boot/rest/ParserController.kt
@@ -2,6 +2,7 @@ package tk.epavlov.microsystem.boot.rest
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import tk.epavlov.microsystem.boot.common.CommonInterface
@@ -9,12 +10,13 @@ import tk.epavlov.microsystem.boot.common.TrackData
 import tk.epavlov.microsystem.boot.parsers.ParserService
 
 @RestController
+@RequestMapping(value = ["tracking"])
 class ParserController : CommonInterface{
 
     @Autowired
     lateinit var parserService: ParserService
 
-    @GetMapping("tracking")
+    @GetMapping
     fun getTimes(@RequestParam("trackId", required = true)  trackId: String): List<TrackData?> {
         return  parserService.getTrackInfo(trackId)
     }
