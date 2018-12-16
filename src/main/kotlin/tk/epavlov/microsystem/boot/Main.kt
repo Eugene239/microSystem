5764851ac6d67b397f1d5cf8bfe0065cd6c1790a
Eugene <evgpavlov18@gmail.com>
2018-12-16 18:53:22 +0300

    change to war build

--- src/main/kotlin/tk/epavlov/microsystem/boot/Main.kt
+++ src/main/kotlin/tk/epavlov/microsystem/boot/Main.kt
@@ -3,10 +3,11 @@ package tk.epavlov.microsystem.boot
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @EnableConfigurationProperties
-class Application
+class Application : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
